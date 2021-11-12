62bf2ae7756470ef2167b264f61567652fc27398
iamtheef <eftihis.riise@gmail.com>
2021-11-12 16:48:40 +0200

    server changes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const compression = require("compression");
 const cors = require("cors");
 const helmet = require("helmet");
 const express = require("express");
-const { isProduction } = require("./utils/isProduction");
 
 require("dotenv").config();
 
@@ -20,29 +19,11 @@ app.use(
 );
 
 const runServer = () => {
-  if (isProduction()) {
-    console.log("PRODUCTION");
-
-    const fs = require("fs");
-    const https = require("https");
-    const p = Number(process.env.prod_PORT);
-
-    const options = {
-      cert: fs.readFileSync(__dirname + "/certs/fullchain.pem", "utf8"),
-      key: fs.readFileSync(__dirname + "/certs/privkey.pem", "utf8"),
-    };
-    const server = https.createServer(options, app);
-    server.listen(p, console.log("Server is on port:", p));
-    return server;
-  } else {
-    console.log("DEVELOPMENT");
-    const http = require("http");
-    const server = http.createServer(app);
-    // connectDB();
-    const p = Number(process.env.dev_PORT);
-    server.listen(p, console.log("Server is on port:", p));
-    return server;
-  }
+  const http = require("http");
+  const server = http.createServer(app);
+  const p = Number(process.env.PORT);
+  server.listen(p, console.log("Server is on port:", p));
+  return server;
 };
 
 module.exports = {

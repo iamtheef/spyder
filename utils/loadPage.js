634bdb9cfb1c861f8bb9ae29b51003921125819f
iamtheef <eftihis.riise@gmail.com>
2021-11-23 17:11:50 +0200

    some test changes

--- utils/loadPage.js
+++ utils/loadPage.js
@@ -22,14 +22,13 @@ const loadWithPuppeteer = async (link) => {
     executablePath: revisionInfo.executablePath,
     args: ["--incognito"],
   });
+  const page = await browser.newPage();
 
   await page.setUserAgent(
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"
   );
 
   await page.setJavaScriptEnabled(true);
-
-  const page = await browser.newPage();
 
   await page.goto(link, {
     waitUntil: "networkidle2",
